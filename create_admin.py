f45b508c0bf8023cdf147e4e409937f06749f198
Turki12 <48205122+Turki12@users.noreply.github.com>
2025-04-22 18:29:15 +0300

    Create create_admin.py
    correct location file

---
+++ create_admin.py
@@ -0,0 +1,18 @@
+# create_admin.py
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    help = 'Creates a superuser if none exist'
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        if not User.objects.filter(is_superuser=True).exists():
+            User.objects.create_superuser(
+                username='admin',
+                email='admin@example.com',
+                password='admin123'
+            )
+            self.stdout.write(self.style.SUCCESS('Superuser created.'))
+        else:
+            self.stdout.write(self.style.WARNING('Superuser already exists.'))
